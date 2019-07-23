3aed50be89b4df30972c04a83c54c09f315619a9
Sanah-Hanif <saneebanee@yahoo.com>
2019-07-23 19:27:09 +0200

    Worked on camera and shooting objects

---
+++ Team4Prototype1/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float horizontalSpeed, verticalSpeed;
+    private float _horizontalMovement, _verticalMovement;
+
+    public float upperX, lowerX, upperY, lowerY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalMovement += horizontalSpeed * Input.GetAxis("Mouse X");
+        _verticalMovement -= verticalSpeed * Input.GetAxis("Mouse Y");
+
+        _horizontalMovement = Mathf.Clamp(_horizontalMovement, lowerX, upperX);
+        //the rotation range
+        _verticalMovement = Mathf.Clamp(_verticalMovement, lowerY, upperY);
+        //the rotation range
+
+        transform.eulerAngles = new Vector3(_verticalMovement, _horizontalMovement, 0.0f);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            WhatchaPointingAt();
+        }
+    }
+
+    void WhatchaPointingAt()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit))
+        {
+            Item it = GameObject.Find("Object Manager").GetComponent<ObjectManager>().FindItemWithGivenObject(hit.transform.gameObject);
+            if (it != null)
+            {
+                print(it.GetName());
+            }
+        }
+    }
+}
---
+++ Team4Prototype1/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    private string itemName;
+    private GameObject obj;
+
+    public Item (string n)
+    {
+        itemName = n;
+    }
+
+    public void SetName(string n)
+    {
+        itemName = n;
+    }
+
+    public void SetObject(GameObject o)
+    {
+        obj = o;
+    }
+
+    public GameObject GetObject()
+    {
+        return obj;
+    }
+
+    public string GetName()
+    {
+        return itemName;
+    }
+}
+
+public class ObjectManager : MonoBehaviour
+{
+    public int numberOfBlocks;
+    public float xUpperLimit, xLowerLimit, yUpperLimit, yLowerLimit;
+    public GameObject blockPrefab;
+
+    private List<Item> items = new List<Item>();
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numberOfBlocks; i++)
+        {
+            float x, y;
+            x = Random.Range(xLowerLimit, xUpperLimit);
+            y = Random.Range(yLowerLimit, yUpperLimit);
+            Item item = new Item(counter.ToString());
+            item.SetObject(Instantiate(blockPrefab, new Vector3(x, y, 15), Quaternion.identity));
+            items.Add(item);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Item FindItemWithGivenObject (GameObject obj)
+    {
+        if (obj == null)
+            return null;
+        foreach(Item it in items)
+        {
+            if (it.GetObject() == obj)
+            {
+                return it;
+            }
+        }
+        return null;
+    }
+}
